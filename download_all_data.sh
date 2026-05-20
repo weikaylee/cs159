5013e6d5c7a79782f2dca01935d68714ccd829eb
Olivia Wang <oliviawang2305@gmail.com>
2026-05-20 14:29:31 -0700

    adjust the script pt2

--- download_all_data.sh
+++ download_all_data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -J "download SEN12MS-CR dataset"   # job name
-#SBATCH --partition=cpu          # Uses standard CPU nodes, much faster queue
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=16G                    # Low memory, just enough to unpack files
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks=5   # number of processor cores (i.e. tasks)
+#SBATCH --gres=gpu:0
+#SBATCH --mem=256G                   # Low memory, just enough to unpack files
 #SBATCH --time=24:00:00              # 2 hours (adjust based on dataset size)
 #SBATCH --mail-user=oywang@caltech.edu
 #SBATCH --mail-type=END,FAIL
