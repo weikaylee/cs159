f34302143774c52b5e40d707a8254995e6465d86
Olivia Wang <oliviawang2305@gmail.com>
2026-05-21 10:28:25 -0700

    modify sh file for verify dataset

--- verify_dataset.sh
+++ verify_dataset.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=256G                   # Low memory, just enough to unpack files
 #SBATCH --time=24:00:00              # 2 hours (adjust based on dataset size)
 #SBATCH --mail-user=oywang@caltech.edu
-#SBATCH --mail-type=START, END,FAIL
+#SBATCH --mail-type=END,FAIL
 #SBATCH --output=download_%j.out 
 
 # Run your original script or commands
