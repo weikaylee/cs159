7b8674cbef41990c5d4467a637f5f8e939e321a2
Olivia Wang <oliviawang2305@gmail.com>
2026-05-22 00:10:15 -0700

    updating slurm file for eval_emrdm.sh

--- train_sen12mscr.sh
+++ train_sen12mscr.sh
@@ -30,6 +30,12 @@ CONDA_ENV="/resnick/groups/perona/oywang/conda_envs/myenv"      # TODO: conda en
 
 set -euo pipefail
 
+# Uncomment the lines below if you need CUDA toolkit on PATH (e.g. to build
+# natten or other extensions from source):
+# module load cuda/12.1.1-gcc-13.2.0-vjpligh
+# export CUDA_HOME=/central/software9/spack/opt/spack/linux-rhel9-broadwell/gcc-13.2.0/cuda-12.1.1-vjplighkhdsgu5uuah4oupe3dtarsz5l
+# export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
 # Activate environment.
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate "${CONDA_ENV}"
