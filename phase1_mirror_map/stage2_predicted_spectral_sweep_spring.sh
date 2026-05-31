5d8a915c6640a18aa64148e15ddcb8323e5a47b1
Olivia Wang <oliviawang2305@gmail.com>
2026-05-30 21:45:20 -0700

    make it batchsize 8

--- phase1_mirror_map/stage2_predicted_spectral_sweep_spring.sh
+++ phase1_mirror_map/stage2_predicted_spectral_sweep_spring.sh
@@ -52,7 +52,7 @@ python "$CODE_DIR/run_loss_ablation_predicted.py" \
     --output_root  "$OUTPUT_ROOT" \
     --roi          ROIs1158_spring \
     --epochs       3 \
-    --batch_size   16 \
+    --batch_size   8 \
     --lr           2e-4 \
     --num_workers  2 \
     --max_sigma    0.1 \
